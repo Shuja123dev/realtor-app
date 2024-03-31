7e2d241c26b1f51c8586c4cd2aeb51121c574c9d
Shuja ud Din <shuja1339@gmail.com>
2024-03-31 16:56:57 +0500

    home filteration

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,14 +1,32 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { CreateHomeDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
 
 @Controller('home')
 export class HomeController {
     constructor(private readonly homeService: HomeService) { };
 
     @Get("")
-    getHomes() {
-        return this.homeService.getHomes();
+    getHomes(
+        @Query("city") city?: string,
+        @Query("minPrice") minPrice?: string,
+        @Query("maxPrice") maxPrice?: string,
+        @Query("propertyType") propertyType?: PropertyType,
+    ) {
+
+        const price = minPrice || maxPrice ? {
+            ...(minPrice && { gte: parseFloat(minPrice) }),
+            ...(maxPrice && { lte: parseFloat(maxPrice) }),
+        } : undefined;
+
+        const filters = {
+            ...(city && { city }),
+            ...(price && { price }),
+            ...(propertyType && { propertyType }),
+        };
+
+        return this.homeService.getHomes(filters);
     }
 
     @Post()
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PropertyType } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+interface Filters {
+    city?: string;
+    price: object;
+    propertyType?: PropertyType
+}
 
 @Injectable()
 export class HomeService {
     constructor(private readonly prismaService: PrismaService) { };
 
-    async getHomes() {
-        return this.prismaService.home.findMany();
+    async getHomes({
+        city,
+        price,
+        propertyType
+    }: Filters) {
+        return this.prismaService.home.findMany({
+            where: {
+                City: city,
+                price,
+                propertyType: propertyType
+            }
+        })
     }
 }
